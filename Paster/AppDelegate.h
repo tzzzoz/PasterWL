0738e8d6221f7647ca35414bfa9e29758fa13d23
tzzzoz <tzzzoz@gmail.com>
2013-03-06 21:17:17 +0800

    Initial Commit

---
+++ Paster/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Paster
+//
+//  Created by tzzzoz on 13-3-6.
+//  Copyright (c) 2013年 Wuxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
